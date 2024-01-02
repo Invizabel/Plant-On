96a69ed7a4c7aad9c15b323f784a23dbb6984b73
Invizabel <46697708+Invizabel@users.noreply.github.com>
2024-01-02 16:12:54 -0600

    Add files via upload

--- Plant On/main.py
+++ Plant On/main.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import time
 import pygame
@@ -11,6 +12,8 @@ sunflower_img = pygame.image.load("assets/sunflower.png").convert_alpha()
 cherrybomb_img = pygame.image.load("assets/cherrybomb.png").convert_alpha()
 walnut_img = pygame.image.load("assets/walnut.png").convert_alpha()
 potatomine_img = pygame.image.load("assets/potatomine.png").convert_alpha()
+my_font = pygame.font.SysFont("calibri", 8)
+cooldown_font = pygame.font.SysFont("calibri", 20)
 pygame.mouse.set_visible(False)
 
 clock = pygame.time.Clock()
@@ -18,16 +21,26 @@ clock = pygame.time.Clock()
 cursor_x = 6
 cursor_y = 6
 
+pea_progress = 0
 player_choice = None
+ticks = 0
+sun = 50
+
 choices = [peashooter_img,sunflower_img,cherrybomb_img,walnut_img,potatomine_img]
+cooldown_key = [8,8,50,30,30]
+new_cooldown_key = cooldown_key[:]
 board = [[None,None,None,None,None,None,None,None,None],
-              [None,None,None,None,None,None,None,None,None],
-              [None,None,None,None,None,None,None,None,None],
-              [None,None,None,None,None,None,None,None,None]]
+         [None,None,None,None,None,None,None,None,None],
+         [None,None,None,None,None,None,None,None,None],
+         [None,None,None,None,None,None,None,None,None]]
 
 while True:
     time.sleep(0.1)
     display.fill("orange")
+    ticks += 1
+    if ticks == 10:
+        ticks = 0
+
     for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -47,8 +60,25 @@ while True:
                         for j,x in enumerate(board):
                             for i,y in enumerate(x):
                                 if i == int(cursor_x / 12) and j == int(cursor_y / 12):
-                                    board[j][i] = player_choice
-                                    player_choice = None
+                                    if sun >= 100 and player_choice == peashooter_img and board[j][i] == None:
+                                        board[j][i] = peashooter_img
+                                        sun -= 100
+
+                                    if sun >= 50 and player_choice == sunflower_img and board[j][i] == None:
+                                        board[j][i] = sunflower_img
+                                        sun -= 50
+
+                                    if sun >= 150 and player_choice == cherrybomb_img and board[j][i] == None:
+                                        board[j][i] = cherrybomb_img
+                                        sun -= 150
+
+                                    if sun >= 50 and player_choice == walnut_img and board[j][i] == None:
+                                        board[j][i] = walnut_img
+                                        sun -= 50
+
+                                    if sun >= 25 and player_choice == potatomine_img and board[j][i] == None:
+                                        board[j][i] = potatomine_img
+                                        sun -= 25
                                             
                 if event.key == K_UP or event.key == K_w:
                     if cursor_y < 58 and cursor_y > 6:
@@ -89,7 +119,68 @@ while True:
         for j,y in enumerate(x):
             if y != None:
                 display.blit(y, ((j * 6) * 2 + 2, (i * 6) * 2 + 2))
-    
-    pygame.draw.circle(display, "black", [cursor_x, cursor_y], 6, 1)    
 
-    pygame.display.flip()
+    # calculate sun
+    sunflower_count = 0
+    for i in board:
+        sunflower_count += i.count(sunflower_img)
+
+    for i in range(0,sunflower_count):
+        my_rand = random.randint(1,240)
+        if my_rand == 1:
+            if sun + 25 < 1000:
+                sun += 25
+
+            else:
+                sun = 999
+
+    my_rand = random.randint(1,100)
+    if my_rand == 1:
+        if sun + 25 < 1000:
+            sun += 25
+
+        else:
+            sun = 999
+    
+
+    # draw ammo
+    pea_bool = True
+    for i,x in enumerate(board):
+        for j,y in enumerate(x):
+            if y == peashooter_img:
+                if pea_bool:
+                    pea_progress += 4
+                    pea_bool = False
+
+                if pea_progress + (j * 6) * 2 + 12 <= 108:
+                    pygame.draw.circle(display, "black", [pea_progress + (j * 6) * 2 + 8, (i * 6) * 2 + 4], 2, 0)
+
+                if pea_progress == 108:
+                    pea_progress = 0
+
+    
+
+    # draw cursor
+    pygame.draw.circle(display, "black", [cursor_x, cursor_y], 6, 1)
+
+    # draw sun text
+    new_sun = ""
+    for i in str(sun):
+        new_sun += i + " "
+
+    sun_font = my_font.render(new_sun, False, "black")
+    display.blit(sun_font, (110, 0))
+
+    # draw cooldown text
+    if ticks == 9:
+        for i,j in enumerate(choices):
+            if  new_cooldown_key[i] - 1 >= 0:
+                new_cooldown_key[i] -= 1
+
+    for i,j in enumerate(choices):
+        if new_cooldown_key[i] != 0:
+            new_cooldown_font = cooldown_font.render("X", False, "black")
+            display.blit(new_cooldown_font, ((i * 12) + 1, 50))
+
+    # refresh
+    pygame.display.update()
